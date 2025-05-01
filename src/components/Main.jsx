77fec16eae0e428badbf13cd203359a8b32e3ccb
Fakiyat <fakiyatafaq14@gmail.com>
2025-05-02 01:09:05 +0530

    Body completed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-
+import meme from "../../images/meme.png";
 function Main() {
   return (
-    <div className="form">
-      <label>
-        TopText
-        <input type="text" placeholder="One does not hold" name="toptext" />
-      </label>
-      <label>
-        bottomText
-        <input type="text" placeholder="Any grudges" name="bottemtext" />
-      </label>
-      <button> Generate new meme image 🖼️</button>
-    </div>
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input type="text" placeholder="One does not hold" name="toptext" />
+        </label>
+        <label>
+          Bottom Text
+          <input type="text" placeholder="Any grudges" name="bottemtext" />
+        </label>
+        <button> Generate new meme image 🖼️</button>
+      </div>
+      <div className="meme">
+        <img src={meme} />
+        <span className="top">One does not hold</span>
+        <span className="bottom">Any grudges</span>
+      </div>
+    </main>
   );
 }
 
