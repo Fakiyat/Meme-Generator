2b09deaa18b942a05efb531daec42f48377c9621
Fakiyat <fakiyatafaq14@gmail.com>
2025-05-03 18:53:48 +0530

    add api for memes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,63 @@
-import React from "react";
-import meme from "../../images/meme.png";
+import React, { useEffect, useState } from "react";
+
 function Main() {
+  const [meme, setMeme] = useState({
+    toptext: "One does not hold",
+    bottomtext: "Any grudegs",
+    randomImage: "../../images/meme.png",
+  });
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  //This fuction is called when the button is click and it will generate a random image form 1 to the max length of the data array
+  function handleMeme() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setAllMemes((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+  function handleInput(event) {
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not hold" name="toptext" />
+          <input
+            type="text"
+            placeholder="One does not hold"
+            name="toptext"
+            onChange={handleInput}
+          />
         </label>
         <label>
           Bottom Text
-          <input type="text" placeholder="Any grudges" name="bottemtext" />
+          <input
+            type="text"
+            placeholder="Any grudges"
+            name="bottomtext"
+            onChange={handleInput}
+          />
         </label>
-        <button> Generate new meme image 🖼️</button>
+        <button onClick={handleMeme}> Generate new meme image 🖼️</button>
       </div>
       <div className="meme">
-        <img src={meme} />
-        <span className="top">One does not hold</span>
-        <span className="bottom">Any grudges</span>
+        <img src={meme.randomImage} />
+        <span className="top">{meme.toptext}</span>
+        <span className="bottom">{meme.bottomtext}</span>
       </div>
     </main>
   );
